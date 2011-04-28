932df3f550a9a00d59a80e3fd8fa0509ce263b43
Gregory Waxman <akkuma+github@gmail.com>
2011-04-28 10:43:29 -0400

    Updated to latest browsers

--- jquery.reject.js
+++ jquery.reject.js
@@ -31,7 +31,7 @@
 			display: [], // What browsers to display and their order (default set below)
 			browserInfo: { // Settings for which browsers to display
 				firefox: {
-					text: 'Firefox 3.6', // Text below the icon
+					text: 'Firefox 4', // Text below the icon
 					url: 'http://www.mozilla.com/firefox/' // URL For icon/text link
 				},
 				safari: {
@@ -43,11 +43,11 @@
 					url: 'http://www.opera.com/download/'
 				},
 				chrome: {
-					text: 'Chrome 9+',
+					text: 'Chrome 11+',
 					url: 'http://www.google.com/chrome/'
 				},
 				msie: {
-					text: 'Internet Explorer 8',
+					text: 'Internet Explorer 9',
 					url: 'http://www.microsoft.com/windows/Internet-explorer/'
 				},
 				gcf: {
